7f77567d524c77167f158ad5113ffbf9e1685f45
hangyejiadao <953917332@qq.com>
2018-03-26 17:53:32 +0800

    123123

--- WinForm/Helper/SqlHelper.cs
+++ WinForm/Helper/SqlHelper.cs
@@ -1,6 +1,8 @@
 ﻿using Model.BaseModel;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,35 +11,96 @@ namespace Helper
 {
     public class SqlHelper
     {
-        public string GetInsertSql<T>(T t) where T : Entity
-        {
-            StringBuilder sbFiled = new StringBuilder("(");
-            StringBuilder sbValues = new StringBuilder("(");
-            foreach (var item in typeof(T).GetProperties())
-            {
+        private static LogHelper Log = new LogHelper(typeof(SqlHelper));
 
-                object[] obj = item.GetCustomAttributes(typeof(KeyAttribute), true);
-                if (obj.Length > 0)
+        private static readonly string Constr = ConfigurationManager.ConnectionStrings["Constr"].ToString();
+
+
+
+        public static async Task<bool> Execute(string sql)
+        {
+            SqlTransaction transaction = null;
+            try
+            {
+                using (SqlConnection con = new SqlConnection(Constr))
                 {
-                    if (item is int || item is Int32 || item is long || item is Int64 || item is Int16)
+                    if (con.State != System.Data.ConnectionState.Open)
                     {
-                        continue;
+                        await con.OpenAsync();
+                    }
+
+                    SqlCommand cmd = con.CreateCommand();
+
+                    //启动事务
+                    transaction = con.BeginTransaction("Transaction");
+                    //设定SqlCommand的事务和链接对象
+                    cmd.Connection = con;
+                    cmd.Transaction = transaction;
+                    cmd.CommandText = sql;
+                    int resl = cmd.ExecuteNonQuery();
+                    if (resl > 0)
+                    {
+                        transaction.Commit();
+                        return true;
+                    }
+                    else
+                    {
+                        transaction.Rollback();
+                        return false; 
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                transaction.Rollback();
+                Log.Error(e.ToString());
+                return false;
+            }
+
+        }
+
+
+
+        protected static string GetInsertSql<T>(T t) where T : Entity
+        {
+            try
+            {
+                StringBuilder sbFiled = new StringBuilder("(");
+                StringBuilder sbValues = new StringBuilder("(");
+                foreach (var item in typeof(T).GetProperties())
+                {
+
+                    object[] obj = item.GetCustomAttributes(typeof(KeyAttribute), true);
+                    if (obj.Length > 0)
+                    {
+                        if (item is int || item is Int32 || item is long || item is Int64 || item is Int16)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            sbFiled.Append(item.Name + ",");
+                            sbValues.Append("'" + item.GetValue(t) + "',");
+                        }
                     }
                     else
                     {
                         sbFiled.Append(item.Name + ",");
-                        sbValues.Append("'" + item.GetValue(t) + "',");
+                        sbValues.Append(item.GetValue(t) + "',");
                     }
                 }
-                else
-                {
-                    sbFiled.Append(item.Name + ",");
-                    sbValues.Append(item.GetValue(t) + "',");
-                }
+
+                string sql = "INSERT " + typeof(T).Name + sbFiled.ToString().TrimEnd(',') + ")" + "values " + sbValues.ToString().TrimEnd(',') + ")";
+                return sql;
+            }
+            catch (Exception e)
+            {
+
+                Log.Error(e.ToString());
+                return e.ToString();
             }
 
-            string sql = "INSERT " + typeof(T).Name + sbFiled.ToString().TrimEnd(',') + ")" + "values " + sbValues.ToString().TrimEnd(',') + ")";
-            return sql;
         }
     }
 }
